9878a5fad01edce015e1b2e2cd5458920acb12e2
NicRiv <nico.a.rivera3@gmail.com>
2023-01-28 09:27:31 -0300

    newArr deleted

--- index.js
+++ index.js
@@ -3,16 +3,15 @@
  */
 
 const fnCases = (arr) => {
-    arr = arr.slice(0)
-    const newArr = arr.map(item => [JSON.stringify(item[0]), JSON.stringify(item[1])])
-    const TOTAL_CASES = newArr.length;
+    arr = arr.map(item => [JSON.stringify(item[0]), JSON.stringify(item[1])])
+    const TOTAL_CASES = arr.length;
 
-    const solved = newArr.every(fn => fn[0] === fn[1])
+    const solved = arr.every(fn => fn[0] === fn[1])
 
     if (solved) {
         return console.log(`All test passed: (${TOTAL_CASES}/${TOTAL_CASES})`)
     } else {
-        newArr.map((fn, idx) => {
+        arr.map((fn, idx) => {
             if (fn[0] !== fn[1]) {
                 return console.log(`Test case ${idx + 1} fail.`)
             }
