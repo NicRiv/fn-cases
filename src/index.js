89f836087915d9e902e7cbc6b671c095e86b754f
NicRiv <nico.a.rivera3@gmail.com>
2023-02-02 19:08:18 -0300

    boolean option added, new output message

--- src/index.js
+++ src/index.js
@@ -2,20 +2,24 @@
  * fn-cases
  */
 
-const fnCases = (arr) => {
+const fnCases = (arr, bool = false) => {
     arr = arr.map(item => [JSON.stringify(item[0]), JSON.stringify(item[1])])
     const TOTAL_CASES = arr.length;
-
     const solved = arr.every(fn => fn[0] === fn[1])
+
+    if (bool) return solved
 
     if (solved) {
         return console.log(`All test passed: (${TOTAL_CASES}/${TOTAL_CASES})`)
     } else {
+        let caseFail = 0
         arr.map((fn, idx) => {
             if (fn[0] !== fn[1]) {
-                return console.log(`Test case ${idx + 1} fail.`)
+                caseFail++
+                return console.log(`Test case ${idx + 1} fail`)
             }
         })
+        console.log(`(${TOTAL_CASES - caseFail}/${TOTAL_CASES}) test cases passed`)
     }
 }
 
